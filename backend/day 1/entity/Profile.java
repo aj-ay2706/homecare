143ba3c5f13392e672795b571788162628483eab
aj-ay2706 <118709615+aj-ay2706@users.noreply.github.com>
2024-02-27 16:30:01 +0530

    Add files via upload

---
+++ backend/day 1/entity/Profile.java
@@ -0,0 +1,41 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="register")
+
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="Name",nullable=false)
+    private String name;
+
+    @Column(name="Email",nullable=false,unique=true)
+    private String email;
+
+    @Column(name="Gender",nullable=false)
+    private String gender;
+
+    @Column(name="Age",nullable=false)
+    private Long age;
+
+    @Lob
+    @Column(name="Image",nullable = false)
+    private byte[] image;
+}
