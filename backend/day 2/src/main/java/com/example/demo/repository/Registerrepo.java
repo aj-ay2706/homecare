f11e727ac7a7f37385759a4a27ffeccdf623986e
aj-ay2706 <118709615+aj-ay2706@users.noreply.github.com>
2024-02-29 16:16:12 +0530

    Delete backend/day 2 directory

--- backend/day 2/src/main/java/com/example/demo/repository/Registerrepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-import com.example.demo.entity.Register;
-public interface Registerrepo extends JpaRepository<Register, Long> {
-     Optional<Register> findByName(String name);
-}
